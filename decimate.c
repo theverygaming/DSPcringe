65a40af35fc5f41958a907131b53bb0d18d2ca7b
theverygaming <example@example.com>
2021-12-06 10:05:24 +0100

    Remove thingy that was for testing

--- decimate.c
+++ decimate.c
@@ -45,7 +45,7 @@ void main()
 	for (int i = 0; i < samp_count; i++)
 	{
 		inputComplex[(2*i)] = samples[i];
-		inputComplex[(2*i)+1] = 1.5;
+		inputComplex[(2*i)+1] = 0;
 	}
 	for (int i = 0; i < 100; i++)
 	{
